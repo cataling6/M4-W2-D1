42f2685e14e24ec761c19403ea9e3a011211b460
Catalin Radoi <114146323+cataling6@users.noreply.github.com>
2024-01-09 19:59:26 +0100

    up

--- script.js
+++ script.js
@@ -17,13 +17,15 @@ const fetchPhotos = () => {
     .then((imgs) => {
       for (let i = 0; i < imgs.photos.length; i++) {
         //console.log(imgs.photos[i]);
-        card.innerHTML += ` <img src="${imgs.photos[i].src.medium}" class="card-img-top" alt="..." />
+        card.innerHTML += `
+        <div class="card">
+        <img src="${imgs.photos[i].src.medium}" class="card-img-top" alt="..." />
           <div class="card-body">
             <h5 class="card-title">${imgs.photos[i].photographer}</h5>
             <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             b5
-          </div>`;
+          </div></div> `;
       }
     })
     .catch((err) => {
