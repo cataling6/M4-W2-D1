c1efdc8ffe0363daac6ef550a0c64830014cf3f2
Catalin Radoi <radoi.catalin88@gmail.com>
2024-01-10 23:10:12 +0100

    up

--- script.js
+++ script.js
@@ -1,55 +1,125 @@
 let query = document.querySelector("#search");
 let valoreRicerca;
-const textBox = document.querySelector("#search");
+let counterFetch = 0;
 const card = document.querySelector("#immagine");
-let copia = {};
-const fetchPhotos = () => {
-  fetch(`https://api.pexels.com/v1/search?query=${valoreRicerca}`, {
-    headers: {
-      Authorization: "FZRsZAOPwP3pNnVygNbODvcOYnkKNtb3Hpzm3runAtM17yQ0nHiUwu6B",
-    },
-  })
-    .then((resp) => {
-      if (resp.ok) {
-        return resp.json();
-      } else {
-        throw resp.status;
-      }
+const next = document.querySelector("#next");
+const prev = document.querySelector("#prev");
+
+const fetchPhotos = (page) => {
+  if (counterFetch == 0) {
+    fetch(`https://api.pexels.com/v1/search?query=${valoreRicerca}`, {
+      headers: {
+        Authorization: "FZRsZAOPwP3pNnVygNbODvcOYnkKNtb3Hpzm3runAtM17yQ0nHiUwu6B",
+      },
     })
-    .then((imgs) => {
-      const { photos } = imgs;
-      copia = photos;
-      photos.map((photo) => {
-        card.innerHTML += `
-        <div class="card col-lg-3">
-        <img src="${photo.src.medium}" class="card-img-top" alt="..." />          
+      .then((resp) => {
+        if (resp.ok) {
+          return resp.json();
+        } else {
+          throw resp.status;
+        }
+      })
+      .then((imgs) => {
+        //const { photos } = imgs;
+        //mi filtro tutte le foto con id minore a 10000
+        //const result = photos.filter((pic) => pic.id > 100000 && pic.id < 200000);
+        // result.forEach((elemento) => {
+        //   card.innerHTML += `
+        //   <div class="card col-sm-6 col-md-4 col-lg-3">
+        //   <img src="${elemento.src.medium}" class="card-img-top" alt="..." />
+        //   <div class="card-body">
+        //       <h5 class="card-title">${elemento.photographer}</h5>
+        //       <h6 class="card-subtitle mb-2 text-muted">${elemento.alt}</h6>
+        //       <p class="card-text d-flex align-self-end"><a href="${elemento.photographer_url}" target="_blank">Link Ph.<a/></p>
+        //   </div>
+        //     </div> `;
+        // });
+
+        const { photos } = imgs;
+        photos.map((photo) => {
+          card.innerHTML += `
+        <div class="card col-sm-6 col-md-4 col-lg-3">
+        <img src="${photo.src.medium}" class="card-img-top" alt="..." />
         <div class="card-body">
             <h5 class="card-title">${photo.photographer}</h5>
             <h6 class="card-subtitle mb-2 text-muted">${photo.alt}</h6>
             <p class="card-text d-flex align-self-end"><a href="${photo.photographer_url}" target="_blank">Link Ph.<a/></p>
         </div>
           </div> `;
+        });
+      })
+      .catch((err) => {
+        console.error(err);
       });
+    counterFetch++;
+  } else {
+    fetch(`https://api.pexels.com/v1/search?query=${valoreRicerca}&page=${page}`, {
+      headers: {
+        Authorization: "FZRsZAOPwP3pNnVygNbODvcOYnkKNtb3Hpzm3runAtM17yQ0nHiUwu6B",
+      },
     })
-    .catch((err) => {
-      console.error(err);
-    });
+      .then((resp) => {
+        if (resp.ok) {
+          return resp.json();
+        } else {
+          throw resp.status;
+        }
+      })
+      .then((imgs) => {
+        //const { photos } = imgs;
+        //mi filtro tutte le foto con id minore a 10000
+        //const result = photos.filter((pic) => pic.id > 100000 && pic.id < 200000);
+        // result.forEach((elemento) => {
+        //   card.innerHTML += `
+        //   <div class="card col-sm-6 col-md-4 col-lg-3">
+        //   <img src="${elemento.src.medium}" class="card-img-top" alt="..." />
+        //   <div class="card-body">
+        //       <h5 class="card-title">${elemento.photographer}</h5>
+        //       <h6 class="card-subtitle mb-2 text-muted">${elemento.alt}</h6>
+        //       <p class="card-text d-flex align-self-end"><a href="${elemento.photographer_url}" target="_blank">Link Ph.<a/></p>
+        //   </div>
+        //     </div> `;
+        // });
+
+        const { photos } = imgs;
+        photos.map((photo) => {
+          card.innerHTML += `
+        <div class="card col-sm-6 col-md-4 col-lg-3">
+        <img src="${photo.src.medium}" class="card-img-top" alt="..." />
+        <div class="card-body">
+            <h5 class="card-title">${photo.photographer}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">${photo.alt}</h6>
+            <p class="card-text d-flex align-self-end"><a href="${photo.photographer_url}" target="_blank">Link Ph.<a/></p>
+        </div>
+          </div> `;
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
 };
 
-textBox.addEventListener("keypress", function (event) {
+query.addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
     cercaImmagine();
   }
 });
+
 function cercaImmagine() {
   card.innerHTML = "";
   valoreRicerca = query.value;
   fetchPhotos();
 }
 
-function costante() {
-  const persona = [1, 2, 3];
-  const p = persona;
-  p[0] = 9;
-  console.log(p, persona);
+function nextPage() {
+  card.innerHTML = "";
+  counterFetch++;
+  fetchPhotos(counterFetch);
+}
+
+function prevPage() {
+  card.innerHTML = "";
+  counterFetch--;
+  fetchPhotos(counterFetch);
 }
