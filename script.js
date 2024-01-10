3b21dd9ed2168fe41e1b4e0c3080d4fdc75aeba6
Catalin Radoi <radoi.catalin88@gmail.com>
2024-01-10 20:55:17 +0100

    up

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let query = document.querySelector("#search");
 let valoreRicerca;
+const textBox = document.querySelector("#search");
 const card = document.querySelector("#immagine");
+let copia = {};
 const fetchPhotos = () => {
   fetch(`https://api.pexels.com/v1/search?query=${valoreRicerca}`, {
     headers: {
@@ -15,20 +17,8 @@ const fetchPhotos = () => {
       }
     })
     .then((imgs) => {
-      // for (let i = 0; i < imgs.photos.length; i++) {
-      //   //console.log(imgs.photos[i]);
-      //   card.innerHTML += `
-      //   <div class="card">
-      //   <img src="${imgs.photos[i].src.medium}" class="card-img-top" alt="..." />
-      //     <div class="card-body">
-      //       <h5 class="card-title">${imgs.photos[i].photographer}</h5>
-      //       <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-      //       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      //       b5
-      //     </div></div> `;
-      // }
-      // console.log(typeof imgs, imgs);
       const { photos } = imgs;
+      copia = photos;
       photos.map((photo) => {
         card.innerHTML += `
         <div class="card col-lg-3">
@@ -37,8 +27,7 @@ const fetchPhotos = () => {
             <h5 class="card-title">${photo.photographer}</h5>
             <h6 class="card-subtitle mb-2 text-muted">${photo.alt}</h6>
             <p class="card-text d-flex align-self-end"><a href="${photo.photographer_url}" target="_blank">Link Ph.<a/></p>
-            
-          </div>
+        </div>
           </div> `;
       });
     })
@@ -47,6 +36,11 @@ const fetchPhotos = () => {
     });
 };
 
+textBox.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    cercaImmagine();
+  }
+});
 function cercaImmagine() {
   card.innerHTML = "";
   valoreRicerca = query.value;
